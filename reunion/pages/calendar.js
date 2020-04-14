50ebb3a637b4d6034a314af95341dd6fcada47cf
Jake Acosta <jakeacosta@Jakes-MBP.attlocal.net>
2020-04-14 10:34:38 -0700

    remove proj

--- reunion/pages/calendar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import Calendar from '../components/Calendar/Calendar';
-import Page from '../components/Page';
-
-export default function CalendarPage() {
-  return <Page Component={Calendar} />;
-}
--- reunion/pages/home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import Home from '../components/Home/Home';
-import Page from '../components/Page';
-
-export default function HomePage() {
-  return <Page Component={Home} />;
-}
